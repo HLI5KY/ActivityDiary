c655bc36b778367134f3c80cdb1913b233789ecf
UglyRabb1t <tangzhe_0108@163.com>
2023-12-28 21:18:24 +0800

    onReceive() is called when phone setting changes.

--- app/src/main/java/de/rampro/activitydiary/helpers/ConditionInfo.java
+++ app/src/main/java/de/rampro/activitydiary/helpers/ConditionInfo.java
@@ -210,13 +210,13 @@ public class ConditionInfo{
     public static class GPS extends BroadcastReceiver{
         @Override
         public void onReceive(Context context, Intent intent){
-            // 该方法会在手机设置开关 Location 服务时自动回调；但只会get默认值
-            //
+            // 该方法会在绑定一个gps后、手机设置开关 Location 服务时自动回调
             // Log.d("Location_Check", "onReceive可执行");
             String action = intent.getAction();
             switch (action) {
                 case LocationManager.MODE_CHANGED_ACTION:
-                    if (intent.getBooleanExtra(LocationManager.EXTRA_LOCATION_ENABLED, false)){
+                    // if (intent.getBooleanExtra(LocationManager.EXTRA_LOCATION_ENABLED, false)){
+                    if (LocationHelper.helper.locationManager.isLocationEnabled()){
                         Log.d("Location_Info", "定位可用");
                     }
                     else{
@@ -229,6 +229,8 @@ public class ConditionInfo{
                     // 暂时没有比较好的实现，这一段很可能是冗余的。
                     String providerName = intent.getStringExtra(LocationManager.EXTRA_PROVIDER_NAME);
                     if (intent.getBooleanExtra(LocationManager.EXTRA_PROVIDER_ENABLED, true)){
+                    // if (LocationHelper.helper.locationManager.isProviderEnabled(providerName)){
+                    // 该方法会导致崩溃，原因未知
                         Log.d("LocationProvider_Info", "Provider: " + providerName + " 可用");
                     }
                     else{
